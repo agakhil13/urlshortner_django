433380f1da6cb71c51955973c93adce1b86e617e
agakhil13 <aguptaji1310@gmail.com>
2022-04-18 17:14:16 +0530

    settings.py

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -79,7 +79,12 @@ WSGI_APPLICATION = 'urlshortner.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config()
 }
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
