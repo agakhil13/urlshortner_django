67cf407a89b44b94f9c2dab3b83f4aaf94d73e6e
agakhil13 <aguptaji1310@gmail.com>
2022-04-18 14:36:51 +0530

    Shortning completed

--- shortner/views.py
+++ shortner/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+import uuid
+from .models import Url
+from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def create(request):
+    if request.method == 'POST':
+        url = request.POST['link']
+        if not Url.objects.filter(link=url).exists():
+            uid = str(uuid.uuid4())[:5]
+            new_url = Url(link=url,uuid=uid)
+            new_url.save()
+        else:
+            url_details = Url.objects.get(link=url)
+            uid = url_details.uuid
+        return HttpResponse(uid)
+
+def go(request, pk):
+    url_details = Url.objects.get(uuid=pk)
+    return redirect(url_details.link)
+
